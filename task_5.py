5ca0821496e77ecdd5a3da863b233afa725c69b2
Zhao Sen <zazulinsemen@gmail.com>
2026-01-09 01:49:15 +0300

    task 5 done

---
+++ task_5.py
@@ -0,0 +1,42 @@
+class Results():
+    def __init__(self, victories, draws, losses):
+        self.victories = victories
+        self.draws = draws
+        self.looses = losses
+
+class Football(Results):
+    def number_of_wins(self):
+        return f"Футбольных побед: {self.victories}"
+    
+    def number_of_draws(self):
+        return f"Футбольных ничьих: {self.draws}"
+    
+    def number_of_losses(self):
+        return f"Футбольных поражений: {self.looses}"
+    
+    def total_points(self):
+        score = 3 * self.victories + self.draws  
+        return f"Общее количество очков: {score}"
+    
+class Hockey(Results):
+    def number_of_wins(self):
+        return f"Хоккейных побед: {self.victories}"
+    
+    def number_of_draws(self):
+        return f"Хоккейных ничьих: {self.draws}"
+    
+    def number_of_losses(self):
+        return f"Хоккейных поражений: {self.looses}"
+    
+    def total_points(self):
+        score = 2 * self.victories + self.draws  
+        return f"Общее количество очков: {score}"
+
+football_team = Football(2,2,2)
+hockey_team = Hockey(2,2,2)
+
+for team in (football_team, hockey_team):
+    print(team.number_of_wins())
+    print(team.number_of_losses())
+    print(team.number_of_draws())
+    print(team.total_points())
